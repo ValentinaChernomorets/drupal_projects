b0cb9865d8bb0eb1858809aa0b85733a69d04081
Valentina Chernomolrets <vcapustina8@gmail.com>
2026-02-15 11:09:50 +0200

    Fix style

---
+++ web/themes/transformyou/js/language-switcher.js
@@ -0,0 +1,26 @@
+(function (Drupal, once) {
+    Drupal.behaviors.principlesPopup = {
+      attach: function (context) {
+        once('language-switcher','.language-switcher-wrapper', context).forEach(wrapper => {
+          const selected = wrapper.querySelector('.selected-language');
+          const list = wrapper.querySelector('.language-switcher')
+          if (!selected || !list) return;
+          selected.addEventListener('click', e => {
+            e.stopPropagation();
+            list.classList.toggle('open');
+            list.style.display = list.style.display === 'block' ? 'none' : 'block';
+          });
+          list.querySelectorAll('li').forEach(item => {
+            item.addEventListener('click', () => {
+              list.classList.remove('open');
+              window.location = item.dataset.value;
+            });
+          });
+          document.addEventListener('click', () => {
+            list.classList.remove('open');
+            list.style.display = 'none';
+          });
+        });
+      }
+    };
+  })(Drupal, once);
